e63f4e96dd922a5c258ed4fff5f726e651dbdd92
Diyash-18 <diyashabnam423gmail.com>
2026-08-06 02:14:53 +0530

    Prepare app for Render deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 from routes.movie_routes import movie_bp
@@ -126,8 +127,16 @@ def edit_movie_page(movie_id):
 # Run Flask
 # ==========================================
 
+# ==========================================
+# Run Flask
+# ==========================================
+
 if __name__ == "__main__":
 
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
-        debug=True
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
